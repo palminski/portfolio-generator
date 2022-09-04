a68d84448d0debbde42c69744a943405e422da9a
palminski <williambolls98@gmail.com>
2022-09-04 11:44:02 -0700

    using inquirer

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-const fs = require ("fs");
-const generatePage = require("./src/page-template.js");
+const inquirer = require('inquirer');
+// const fs = require ("fs");
+// const generatePage = require("./src/page-template.js");
 
-const profileDataArgs = process.argv.slice(2,process.argv.length);
-const [userName,github] = profileDataArgs;
+// const pageHTML = generatePage(userName,github);
 
 
-fs.writeFile('index.html',generatePage(userName,github), err => {
-    if (err) throw err;
-    console.log("Portfolio complete! check the index.html to see output");
-});
+// fs.writeFile('./index.html',pageHTML, err => {
+//     if (err) throw err;
+//     console.log("Portfolio complete! check the index.html to see output");
+// });
+inquirer
+    .prompt ([
+        {
+            type: "input",
+            name: "name",
+            message: "what is your name?"
+        }
+    ])
+    .then(answers => console.log(answers));
