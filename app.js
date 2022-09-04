a62e14761bb76b83bd7f02f6fa635c558d9d7151
palminski <williambolls98@gmail.com>
2022-09-04 10:36:12 -0700

    added the ability to generate a webpage and split code into 2 files

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+const fs = require ("fs");
+const generatePage = require("./src/page-template.js");
+
 const profileDataArgs = process.argv.slice(2,process.argv.length);
+const [userName,github] = profileDataArgs;
 
 
-const printProfileData = (profileDataArr) => {
-
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs)
+fs.writeFile('index.html',generatePage(userName,github), err => {
+    if (err) throw err;
+    console.log("Portfolio complete! check the index.html to see output");
+});
