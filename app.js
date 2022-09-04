22e47bade27adbcc109faeb4972497e707bc5b28
palminski <williambolls98@gmail.com>
2022-09-04 12:17:20 -0700

    added ability to store multiple projects

--- app.js
+++ app.js
@@ -9,12 +9,91 @@ const inquirer = require('inquirer');
 //     if (err) throw err;
 //     console.log("Portfolio complete! check the index.html to see output");
 // });
-inquirer
+
+    
+
+const promptUser = () => {
+    return inquirer
     .prompt ([
         {
             type: "input",
             name: "name",
-            message: "what is your name?"
+            message: "What is your name?"
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "What is your Github Username?"
+        },
+        {
+            type: "input",
+            name: "about",
+            message: "Please provide some information about yourself"
         }
-    ])
-    .then(answers => console.log(answers));
+    ]);
+};
+
+const promptProjects = portfolioData => {
+
+    
+
+    if (!portfolioData.projects) {
+        portfolioData.projects = [];
+    };
+
+    console.log(`
+    =================
+    Add a New Project
+    =================
+    `);
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is the name of your project?"
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Provide a description for your project (required)"
+        },
+        {
+            type: "checkbox",
+            name: "languages",
+            message: "What did you build it with?",
+            choices: ["JavaScript","HTML","CSS","ES6","jQuerry", "Bootstrap","Node"]
+        },
+        {
+            type: "input",
+            name: "link",
+            message: "Enter github URL (required)"
+        },
+        {
+            type: "confirm",
+            name: "feature",
+            message: "Would you like to feature this project?",
+            default: false
+        },
+        {
+            type: "confirm",
+            name: "confirmAddProject",
+            message: "Would you like to enter another project?",
+            default: false
+        }
+    ]).then(projectData => {
+        portfolioData.projects.push(projectData);
+        if (projectData.confirmAddProject) {
+            return promptProjects(portfolioData);
+        }
+        else
+        {
+            return portfolioData;
+        }
+    });
+};
+
+promptUser()
+            .then(promptProjects)
+            .then(portfolioData => {
+                console.log(portfolioData);
+            });
